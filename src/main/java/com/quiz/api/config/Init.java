e0e52dc7429c6ecebafb97a9cc2d66990ab4f212
Adam_Z <youngtoss38@gmail.com>
2020-01-03 10:56:53 +0100

    switched to localdb for now

--- src/main/java/com/quiz/api/config/Init.java
+++ src/main/java/com/quiz/api/config/Init.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.util.Set;
 
-//@Component
+@Component
 public class Init {
 
     @Autowired
@@ -25,7 +25,7 @@ public class Init {
     @Autowired
     private ChoiceRepository choiceRepository;
 
-  //  @PostConstruct
+    @PostConstruct
     public void initData(){
 
         Choice choice_1 = new Choice();
@@ -79,14 +79,262 @@ public class Init {
 
         questionRepository.save(question_b);
 
+        choice_b1 = new Choice();
+        choice_b1.setValue("26");
 
-        
+        choice_b2 = new Choice();
+        choice_b2.setValue("27");
+
+        choice_b3 = new Choice();
+        choice_b3.setValue("28");
+
+        choice_b4 = new Choice();
+        choice_b4.setValue("29");
+
+        choiceRepository.save(choice_b1);
+        choiceRepository.save(choice_b2);
+        choiceRepository.save(choice_b3);
+        choiceRepository.save(choice_b4);
+
+        answer_b = new Answer();
+        answer_b.setCorrect(choice_b3);
+
+        answerRepository.save(answer_b);
+
+        question_b = new Question();
+        question_b.setAnswer(answer_b);
+        question_b.setChoices(Set.of(choice_b1,choice_b2,choice_b3,choice_b4));
+        question_b.setCategory(Category.Knowledge);
+        question_b.setValue("How many days February has in 1992?");
+
+        questionRepository.save(question_b);
 
 
 
+        choice_b1 = new Choice();
+        choice_b1.setValue("Linus Torvalds");
+
+        choice_b2 = new Choice();
+        choice_b2.setValue("Bill Gates");
+
+        choice_b3 = new Choice();
+        choice_b3.setValue("Steve Jobs");
+
+        choice_b4 = new Choice();
+        choice_b4.setValue("Mark Zuckenberg");
+
+        choiceRepository.save(choice_b1);
+        choiceRepository.save(choice_b2);
+        choiceRepository.save(choice_b3);
+        choiceRepository.save(choice_b4);
+
+        answer_b = new Answer();
+        answer_b.setCorrect(choice_b1);
+
+        answerRepository.save(answer_b);
+
+        question_b = new Question();
+        question_b.setAnswer(answer_b);
+        question_b.setChoices(Set.of(choice_b1,choice_b2,choice_b3,choice_b4));
+        question_b.setCategory(Category.It);
+        question_b.setValue("Who create LINUX?");
+
+        questionRepository.save(question_b);
+
+
+        //
+
+        choice_b1 = new Choice();
+        choice_b1.setValue("James Gosling");
+
+        choice_b2 = new Choice();
+        choice_b2.setValue("Dennis Ritchie");
+
+        choice_b3 = new Choice();
+        choice_b3.setValue("Niklaus Wirth");
+
+        choice_b4 = new Choice();
+        choice_b4.setValue("Bjarne Stroustrup");
+
+        choiceRepository.save(choice_b1);
+        choiceRepository.save(choice_b2);
+        choiceRepository.save(choice_b3);
+        choiceRepository.save(choice_b4);
+
+        answer_b = new Answer();
+        answer_b.setCorrect(choice_b4);
+
+        answerRepository.save(answer_b);
+
+        question_b = new Question();
+        question_b.setAnswer(answer_b);
+        question_b.setChoices(Set.of(choice_b1,choice_b2,choice_b3,choice_b4));
+        question_b.setCategory(Category.It);
+        question_b.setValue("Who create C++?");
+
+        questionRepository.save(question_b);
+
+
+        choice_b1 = new Choice();
+        choice_b1.setValue("James Gosling");
+
+        choice_b2 = new Choice();
+        choice_b2.setValue("Dennis Ritchie");
+
+        choice_b3 = new Choice();
+        choice_b3.setValue("Niklaus Wirth");
+
+        choice_b4 = new Choice();
+        choice_b4.setValue("Bjarne Stroustrup");
+
+        choiceRepository.save(choice_b1);
+        choiceRepository.save(choice_b2);
+        choiceRepository.save(choice_b3);
+        choiceRepository.save(choice_b4);
+
+        answer_b = new Answer();
+        answer_b.setCorrect(choice_b1);
+
+        answerRepository.save(answer_b);
+
+        question_b = new Question();
+        question_b.setAnswer(answer_b);
+        question_b.setChoices(Set.of(choice_b1,choice_b2,choice_b3,choice_b4));
+        question_b.setCategory(Category.It);
+        question_b.setValue("Who create Java?");
+        questionRepository.save(question_b);
 
 
 
+        choice_b1 = new Choice();
+        choice_b1.setValue("James Gosling");
+
+        choice_b2 = new Choice();
+        choice_b2.setValue("Dennis Ritchie");
+
+        choice_b3 = new Choice();
+        choice_b3.setValue("Niklaus Wirth");
+
+        choice_b4 = new Choice();
+        choice_b4.setValue("Bjarne Stroustrup");
+
+        choiceRepository.save(choice_b1);
+        choiceRepository.save(choice_b2);
+        choiceRepository.save(choice_b3);
+        choiceRepository.save(choice_b4);
+
+        answer_b = new Answer();
+        answer_b.setCorrect(choice_b2);
+
+        answerRepository.save(answer_b);
+
+        question_b = new Question();
+        question_b.setAnswer(answer_b);
+        question_b.setChoices(Set.of(choice_b1,choice_b2,choice_b3,choice_b4));
+        question_b.setCategory(Category.It);
+        question_b.setValue("Who create C?");
+        questionRepository.save(question_b);
+
+        choice_b1 = new Choice();
+        choice_b1.setValue("James Gosling");
+
+        choice_b2 = new Choice();
+        choice_b2.setValue("Dennis Ritchie");
+
+        choice_b3 = new Choice();
+        choice_b3.setValue("Niklaus Wirth");
+
+        choice_b4 = new Choice();
+        choice_b4.setValue("Bjarne Stroustrup");
+
+        choiceRepository.save(choice_b1);
+        choiceRepository.save(choice_b2);
+        choiceRepository.save(choice_b3);
+        choiceRepository.save(choice_b4);
+
+        answer_b = new Answer();
+        answer_b.setCorrect(choice_b3);
+
+        answerRepository.save(answer_b);
+
+        question_b = new Question();
+        question_b.setAnswer(answer_b);
+        question_b.setChoices(Set.of(choice_b1,choice_b2,choice_b3,choice_b4));
+        question_b.setCategory(Category.It);
+        question_b.setValue("Who create Pascal?");
+        questionRepository.save(question_b);
+
+
+        choice_b1 = new Choice();
+        choice_b1.setValue("Apache License");
+
+        choice_b2 = new Choice();
+        choice_b2.setValue("GNU GPL");
+
+        choiceRepository.save(choice_b1);
+        choiceRepository.save(choice_b2);
+
+
+        answer_b = new Answer();
+        answer_b.setCorrect(choice_b4);
+
+        answerRepository.save(answer_b);
+
+        question_b = new Question();
+        question_b.setAnswer(answer_b);
+        question_b.setChoices(Set.of(choice_b1,choice_b2));
+        question_b.setCategory(Category.Knowledge);
+        question_b.setValue("This license allows the use of source code for both free software and proprietary software.Which one is it?");
+
+        questionRepository.save(question_b);
+
+        choice_b1 = new Choice();
+        choice_b1.setValue("2005");
+        choice_b2 = new Choice();
+        choice_b2.setValue("2003");
+        choice_b3 = new Choice();
+        choice_b3.setValue("2004");
+        choice_b4 = new Choice();
+        choice_b4.setValue("2002");
+
+        choiceRepository.save(choice_b1);
+        choiceRepository.save(choice_b2);
+        choiceRepository.save(choice_b3);
+        choiceRepository.save(choice_b4);
+
+        answer_b = new Answer();
+        answer_b.setCorrect(choice_b3);
+
+        answerRepository.save(answer_b);
+
+        question_b = new Question();
+        question_b.setAnswer(answer_b);
+        question_b.setChoices(Set.of(choice_b1,choice_b2,choice_b3,choice_b4));
+        question_b.setCategory(Category.It);
+        question_b.setValue("Year of facebook first release?");
+        questionRepository.save(question_b);
+
+
+        choice_b1 = new Choice();
+        choice_b1.setValue("Yes");
+        choice_b2 = new Choice();
+        choice_b2.setValue("No");
+
+        choiceRepository.save(choice_b1);
+        choiceRepository.save(choice_b2);
+
+        answer_b = new Answer();
+        answer_b.setCorrect(choice_b2);
+
+        answerRepository.save(answer_b);
+
+        question_b = new Question();
+        question_b.setAnswer(answer_b);
+        question_b.setChoices(Set.of(choice_b1,choice_b2));
+        question_b.setCategory(Category.It);
+        question_b.setValue("Did Steve Jobs wrote at least one line of code in his entire life?");
+
+        questionRepository.save(question_b);
 
     }
 
